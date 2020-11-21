8b37af8fcc999bde2619cb27c7add07a582690ce
hyeonbaekong <carpedm43@gmail.com>
2020-11-21 23:49:33 +0900

    add pip setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name="shrm",
+    version="0.0.1",
+    description = 'shrm',
+    install_requires=["numpy", "scipy", "torch"],
+    author='hyeonbaekong',
+    author_email='carpedm43@gmail.com',
+    license='MIT',
+    python_requires = '>=3',
+    packages=[],
+    package_data={
+    },
+    zip_safe=False,
+
+)
+
