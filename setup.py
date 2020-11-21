f739969665bc31ee9cc4879554a58b432314fab1
hyeonbaekong <carpedm43@gmail.com>
2020-11-22 02:26:02 +0900

    modify add package

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 setup(
     name="shrm",
     version="0.0.2",
-    description = 'shrm',
+    description='shrm',
     install_requires=["numpy", "scipy", "torch"],
     author='hyeonbaekong',
     author_email='carpedm43@gmail.com',
     license='MIT',
-    python_requires = '>=3',
-    packages=[],
+    python_requires='>=3',
+    packages=["shrm"],
     package_data={
         "shrm": [
             "data/*"
@@ -18,4 +18,3 @@ setup(
     zip_safe=False,
 
 )
-
