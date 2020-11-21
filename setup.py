4cb8855b5d04ae6d3c13a40bbc5e309864646d44
hyeonbaekong <carpedm43@gmail.com>
2020-11-22 02:38:42 +0900

    modify setup

--- setup.py
+++ setup.py
@@ -1,20 +1,24 @@
 from setuptools import setup
+from setuptools import find_packages
 
 setup(
     name="shrm",
-    version="0.0.2",
+    version="0.0.3",
     description='shrm',
-    install_requires=["numpy", "scipy", "torch"],
+    url='https://github.com/carpedm43/shrm.git',
     author='hyeonbaekong',
     author_email='carpedm43@gmail.com',
     license='MIT',
     python_requires='>=3',
-    packages=["shrm"],
+    packages=find_packages(),
     package_data={
         "shrm": [
             "data/*"
         ]
     },
     zip_safe=False,
-
+    install_requires=
+    ["scipy==1.5.4",
+     "numpy==1.19.4",
+     "torch==1.7.0"]
 )
