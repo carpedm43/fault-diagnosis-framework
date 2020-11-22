fd34882e22d47c97abfd95d3bcdc63c2c3a1a331
hyeonbaekong <carpedm43@gmail.com>
2020-11-22 14:16:20 +0900

    modify version 0.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name="shrm",
-    version="0.0.4",
+    version="0.1.0",
     description='shrm',
     url='https://github.com/carpedm43/shrm.git',
     author='hyeonbaekong',
